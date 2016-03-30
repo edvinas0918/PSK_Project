7e2f531cc97f573862aff22b0b57a0abe8f87ec2
dzhv <dz.vysniauskas@gmail.com>
2016-03-30 21:36:40 +0300

    Pridedu JQuery, RequireJS, Bootstrapo lib'us

---
+++ web/resources/scripts/bootstrapper.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Dziugas on 3/30/2016.
+ */
+define(['lib/bootstrap.min'], function (bootstrap) {
+    function start() {
+        //alert("calling from the bootstrapper, yo");
+    }
+
+    return {
+        start: start
+    };
+});
---
+++ web/resources/scripts/require-config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Dziugas on 3/30/2016.
+ */
+requirejs.config({
+    baseUrl: 'resources/scripts',
+    paths: {
+        lib: '../jsLibs'
+    }
+});
