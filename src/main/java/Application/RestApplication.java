777c0573edb79f2518fd7d8e5ecbfec92239e354
dzhv <dz.vysniauskas@gmail.com>
2016-04-11 18:15:32 +0300

    Fixing HTTP 404 Warnings: added subcontext for REST Apis

---
+++ src/main/java/Application/RestApplication.java
@@ -0,0 +1,11 @@
+package Application;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by Dziugas on 4/11/2016.
+ */
+@ApplicationPath("/rest")
+public class RestApplication extends Application {
+}
