1d3e1183d284192c6899b9d17143c4a877e1a87a
dzhv <dz.vysniauskas@gmail.com>
2016-04-18 20:33:10 +0300

    Grazinu applicationPath REST endpointams, kad nemetytu erroru

---
+++ src/main/java/RestControllers/RestConfig.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RestControllers;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Dziugas
+ */
+@javax.ws.rs.ApplicationPath("rest")
+public class RestConfig extends Application {
+    
+}
