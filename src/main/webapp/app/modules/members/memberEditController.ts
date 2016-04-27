51431bbf71e63d8eea8e3071a9a7b85a68dc306a
dzhv <dz.vysniauskas@gmail.com>
2016-04-27 09:57:34 +0300

    Member can now be edited

--- src/main/webapp/app/modules/members/memberEditController.ts
+++ src/main/webapp/app/modules/members/memberEditController.ts
@@ -21,21 +21,49 @@ module SummerHouses.members {
             private $routeParams: any,
             private $http: any
         ) {
-            this.scope = $scope;
-            this.httpService = $http;
+            this.$scope.editing = false;
+            this.$scope.editable = true;
+            this.$scope.newMember = false;
+
             this.getMember($routeParams.memberID);
 
-            this.scope.saveMember = () => {
-                // this.httpService.delete('rest/clubmember/' + this.scope.member.id).success(() => {
-                //
-                // });
+            this.$scope.saveMember = () => {    // TODO: pagalvot apie email unikaluma
+                if (this.$scope.newMember){
+                    this.$scope.member.id = null;
+                    this.$http.post('rest/clubmember/', this.$scope.member).success(() => {
+                        this.showSuccessMessage();
+                    })
+                } else {
+                    this.$http.put('rest/clubmember/' + this.$scope.member.id, this.$scope.member).success(() => {
+                        this.showSuccessMessage();
+                    });
+                }
+
+                this.$scope.editing = false;
+            }
+
+            this.$scope.editForm = () => {
+                this.$scope.editing = true;
             }
         }
 
         getMember(memberID: string): void{
-            this.httpService.get('/rest/clubmember/' + memberID).success((member: Member, status) => {
-                this.scope.member = member;
-            });
+            if (this.$scope.newMember){
+                this.$scope.member = new Member();
+            } else {
+                this.$http.get('/rest/clubmember/' + memberID).success((member: Member, status) => {
+                    this.$scope.member = member;
+                });
+            }
+        }
+
+        showSuccessMessage(): void{
+            this.$scope.showAlert = true;
+            setTimeout(() => {
+                this.$scope.$apply(() => {
+                    this.$scope.showAlert = false;
+                })
+            }, 4000)
         }
 
     }
