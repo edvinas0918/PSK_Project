1e7d47ba3470b21025f141ef7ffcd7464f0a8dd1
Mindaugas Jucius <mindaugas.jucius@vinted.com>
2016-04-28 17:00:45 +0300

    Fixing typo

--- src/main/java/Entities/Summerhouse.java
+++ src/main/java/Entities/Summerhouse.java
@@ -110,7 +110,6 @@ public class Summerhouse implements Serializable {
 //    public void setBeginPeriod(MonthDay beginPeriod) {
 //        this.beginPeriod = Helpers.dateFromMonthDay(beginPeriod);
 //    }
-Description Description Description Description
     public void setBeginPeriod(Date beginPeriod) {
         this.beginPeriod = beginPeriod;
     }
