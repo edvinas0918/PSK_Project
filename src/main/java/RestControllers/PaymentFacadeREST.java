9680411ed21217245faf5a9979213ee1a21288a3
gintsam <gintsam@gmail.com>
2016-04-28 10:46:01 +0300

    unstaged changes

---
+++ src/main/java/RestControllers/PaymentFacadeREST.java
@@ -0,0 +1,79 @@
+package RestControllers;
+
+import Entities.Payment;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+/**
+ * Created by Gintautas on 4/28/2016.
+ */
+
+@Stateless
+@Path("payments")
+public class PaymentFacadeREST extends AbstractFacade<Payment> {
+
+    @PersistenceContext(unitName = "com.psk_LabanorasFriends_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    public PaymentFacadeREST() {
+        super(Payment.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void create(Payment entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") Integer id, Payment entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Integer id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Payment find(@PathParam("id") Integer id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Payment> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Payment> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+}
---
+++ src/main/webapp/app/modules/payments/paymentModel.ts
@@ -0,0 +1,21 @@
+module SummerHouses.payments {
+
+    export class Payment {
+        public id:number;
+        public paymentDate:Date;
+        public confirmed:boolean;
+        public taxID:Tax;
+        public memberID:Member;
+    }
+
+    export class Tax {
+        public id:number;
+        public name:string;
+    }
+
+    export class Member {
+        public id:number;
+        public firstName:string;
+        public lastName:string;
+    }
+}
