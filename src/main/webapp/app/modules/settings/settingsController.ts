68d3ac70b116823d642cd1869b5e810dab5784bf
dzhv <dz.vysniauskas@gmail.com>
2016-05-28 12:05:18 +0300

    Rezervacijos pradžios datos nustatymas

--- src/main/webapp/app/modules/settings/settingsController.ts
+++ src/main/webapp/app/modules/settings/settingsController.ts
@@ -2,6 +2,8 @@
 ///<reference path="../../../typings/lodash.d.ts"/>
 ///<reference path="settingsModel.ts"/>
 ///<reference path="../members/memberFormFieldModel.ts"/>
+///<reference path="../utilities/utilities.ts"/>
+///<reference path="../../../typings/moment.d.ts"/>
 
 module SummerHouses.settings {
 
@@ -28,6 +30,7 @@ module SummerHouses.settings {
             this.$scope.showAlert = false;
             
             this.$scope.saveSettings = () => {
+                this.formatReservationDateSetting(this.$scope.settings);
                 this.$http.put('rest/settings', this.$scope.settings).success(() => {
                     _.forEach(this.$scope.originalFieldOptions, (field: members.MemberFormField) => {
                         field.visible = this.$scope.formFields[field.fieldName];
@@ -43,11 +46,24 @@ module SummerHouses.settings {
             this.$scope.startEditing = () => {
                 this.$scope.editing = true;
             }
+
+            this.$scope.months = Utilities.monthsSelectOptions();
+            this.$scope.days = Utilities.getIteratingNumberArray(1, 31);
+            this.$scope.reservationStartMonth = this.$scope.months[0];
+            this.$scope.reservationStartDay = 1;
         }
 
         getSettings(): void{
             this.$http.get('/rest/settings').success((settings: Settings[]) => {
                 this.$scope.settings = settings;
+                var startDateSetting = _.find(settings, setting => {
+                    return setting.referenceCode === "reservationStartDate";
+                });
+                if (startDateSetting){
+                    var startDate = moment(startDateSetting.value);
+                    this.$scope.reservationStartMonth = this.$scope.months[startDate.month()];
+                    this.$scope.reservationStartDay = startDate.date();
+                }
             });
         }
 
@@ -67,6 +83,19 @@ module SummerHouses.settings {
                     this.$scope.showAlert = false;
                 })
             }, 4000)
+        }
+
+        formatReservationDateSetting(settings: Settings[]): void{
+            var dateSetting = _.find(settings, setting => {
+                return setting.referenceCode === "reservationStartDate";
+            })
+
+            if (dateSetting){
+                var date = moment().month(this.$scope.reservationStartMonth.key).date(this.$scope.reservationStartDay);
+                dateSetting.value = date.format('YYYY-MM-DD');
+            }
+
+
         }
 
     }
--- src/main/webapp/app/modules/utilities/utilities.ts
+++ src/main/webapp/app/modules/utilities/utilities.ts
@@ -10,6 +10,55 @@ module Utilities{
                 return "Kandidatas";
         }
     }
+
+    export function monthsSelectOptions(): any[]{
+        return [
+            {"key": "0",
+                "value": "Sausis",
+                "days": 31},
+            {"key": "1",
+                "value": "Vasaris",
+                "days": 28},
+            {"key": "2",
+                "value": "Kovas",
+                "days": 31},
+            {"key": "3",
+                "value": "Balandis",
+                "days": 30},
+            {"key": "4",
+                "value": "Gegužė",
+                "days": 31},
+            {"key": "5",
+                "value": "Birželis",
+                "days": 30},
+            {"key": "6",
+                "value": "Liepa",
+                "days": 31},
+            {"key": "7",
+                "value": "Rūgpjutis",
+                "days": 31},
+            {"key": "8",
+                "value": "Rugsėjis",
+                "days": 30},
+            {"key": "9",
+                "value": "Spalis",
+                "days": 31},
+            {"key": "10",
+                "value": "Lapkritis",
+                "days": 30},
+            {"key": "11",
+                "value": "Gruodis",
+                "days": 31}
+        ];
+    }
+
+    export function getIteratingNumberArray(minValue: number, maxValue: number){
+        var arr = [];
+        for (var i = minValue; i <= maxValue; i++) {
+            arr.push(i);
+        }
+        return arr;
+    }
 }
 
 
